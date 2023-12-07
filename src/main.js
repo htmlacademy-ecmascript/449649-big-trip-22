58435567c29680d9ef4404ebd3b88ad53a400df6
Dmitriy Yurkin <dmitriyyurkin-aq@team.asana.com>
2023-12-07 14:35:42 +0100

    created views

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import BriefView from './view/brief-view.js';
+import FilterView from './view/filters-view.js';
+import TripPresenter from './presenter/trip-presenter.js';
+import {RenderPosition, render} from './render.js';
+
+const tripMain = document.querySelector('.trip-main');
+const tripFilters = document.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
+const tripPresenter = new TripPresenter({tripContainer: tripEvents});
+
+render(new BriefView, tripMain, RenderPosition.AFTERBEGIN);
+render(new FilterView(), tripFilters);
+tripPresenter.init();
